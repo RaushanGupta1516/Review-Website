746e370a2a9c7336f306f25df630cdd16801fa7a
Raushan Gupta <raushangupta5597@gmail.com>
2025-02-01 22:32:26 +0530

    updated

---
+++ front-end/tailwind.config.js
@@ -0,0 +1,17 @@
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+      extend: {
+        screens: {
+          'x5': '538px',
+          'x4': '400px'
+        },
+      },
+    },
+    plugins: [],
+  }
