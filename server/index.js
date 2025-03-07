ea68174e59ece0ccd580caebabc95ac50ab74c1d
SSrivastava18 <saurabh18srivastava1@gamil.com>
2025-03-07 10:04:44 +0530

    index setup

---
+++ server/index.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config(); 
+
+const app = express();
+
+// Middleware setup
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
+
+const CONNECTION_URL = process.env.MONGO_URI; 
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL)
+    .then(() => {
+        app.listen(PORT, () => console.log(`Server Running on Port: ${PORT}`));
+    })
+    .catch((error) => console.error(`MongoDB Connection Error: ${error.message}`));
+
+mongoose.set('strictQuery', false);
+
+
+
+
