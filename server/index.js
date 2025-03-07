602abfaeeaef89b51b57d3ff05bf2985ab4262b4
Saurabh Srivastava <163189524+SSrivastava18@users.noreply.github.com>
2025-03-07 10:05:28 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ dotenv.config();
 
 const app = express();
 
-// Middleware setup
+
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
