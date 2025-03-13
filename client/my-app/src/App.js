3ad03f8bc09587e8da23d136df07ebe535f241a0
SSrivastava18 <saurabh18srivastava1@gamil.com>
2025-03-13 18:34:58 +0530

    first page setup

--- client/my-app/src/App.js
+++ client/my-app/src/App.js
@@ -1,12 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Container, AppBar, Typography, Grid,Grow, Box } from '@mui/material';
+import ss from './images/ss.png';
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
+import useStyles from './style';
 
 function App() {
+  const classes = useStyles();
   return (
-    <div>
-      <h1>App</h1>
-      
-    </div>
+   <Container maxWidth="lg">
+      <AppBar position="static" color="inherit" className={classes.appBar}>
+        <Box display="flex" alignItems="center" gap={2}>
+          <Typography className={classes.heading} variant="h2">
+            Stay-Story
+          </Typography>
+          <img className={classes.image} src={ss} alt="stay story" height="60" />
+        </Box>
+      </AppBar>
+        <Grow in>
+          <Container>
+            <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+              <Grid item xs={12} sm={7}>
+                <Posts/>
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                  <Form/>
+                </Grid>
+            </Grid>
+          </Container>
+          
+        </Grow>
+
+   </Container>
   );
 }
 
--- server/index.js
+++ server/index.js
@@ -4,11 +4,15 @@ import cors from 'cors';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
+import postRoutes from './routes/posts.js';
+
 dotenv.config(); 
 
 const app = express();
 
-// Middleware setup
+app.use('/posts', postRoutes);
+
+
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
