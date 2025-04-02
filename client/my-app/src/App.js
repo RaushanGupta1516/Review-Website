e3ed91779087dac00bacd6de481f259f71f941e3
Raushan Gupta <raushangupta5597@gmail.com>
2025-04-02 11:23:27 +0530

    Initial commit for new repo

--- client/my-app/src/App.js
+++ client/my-app/src/App.js
@@ -1,16 +1,6 @@
-<<<<<<< HEAD
+
 
 import './App.css';
-=======
-import React, {useState, useEffect} from 'react';
-import { Container, AppBar, Typography, Grid,Grow, Box } from '@mui/material';
-import ss from './images/ss.png';
-import Posts from './components/Posts/Posts';
-import Form from './components/Form/Form';
-import useStyles from './style';
-import { useDispatch } from 'react-redux';
-import { getPosts } from './actions/posts'; 
->>>>>>> b73f876a94c93ca105c15dc455f6b8aa4918cdba
 
 
 import React from "react";
@@ -25,8 +15,9 @@ import { ToastContainer } from "react-toastify";
 import { StoreContext } from "./StoreContext";
 import { useContext } from "react";
 import { toast } from "react-toastify";
+
 function App() {
-<<<<<<< HEAD
+
     const { token } = useContext(StoreContext);
   function ProtectedRoute({ element }) {
     if (!token) {
@@ -49,41 +40,6 @@ function App() {
                 <Route path="/review/:id" element={<ReviewDetailPage />} />
             </Routes>
     </div>
-=======
-  const [currentId, setCurrentId] = useState(0);
-  const classes = useStyles();
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(getPosts());
-  }, [currentId, dispatch]);
-  
-  return (
-   <Container maxWidth="lg">
-      <AppBar position="static" color="inherit" className={classes.appBar}>
-        <Box display="flex" alignItems="center" gap={2}>
-          <Typography className={classes.heading} variant="h2">
-            Stay-Story
-          </Typography>
-          <img className={classes.image} src={ss} alt="stay story" height="60" />
-        </Box>
-      </AppBar>
-        <Grow in>
-          <Container>
-            <Grid container justify="space-between" alignItems="stretch" spacing={3} color='white'>
-              <Grid item xs={12} sm={7} >
-                <Posts setCurrentId={setCurrentId}/>
-                </Grid>
-                <Grid item xs={12} sm={4}>
-                  <Form currentId={currentId} setCurrentId={setCurrentId}/>
-                </Grid>
-            </Grid>
-          </Container>
-          
-        </Grow>
-
-   </Container>
->>>>>>> b73f876a94c93ca105c15dc455f6b8aa4918cdba
   );
 }
 
--- client/my-app/src/index.js
+++ client/my-app/src/index.js
@@ -1,39 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
-import {thunk} from 'redux-thunk';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-
-import reducers from './reducers';
+import ReactDOM from 'react-dom/client';
 import App from './App';
-<<<<<<< HEAD
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from "react-router-dom";
-import StoreContextProvider from "./StoreContext";
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-      <BrowserRouter>
-		<StoreContextProvider>
-			<App />
-		</StoreContextProvider>
-	</BrowserRouter>
-  </React.StrictMode>
-=======
 import './index.css';
 
-// Create a theme instance
-const theme = createTheme();
+import { BrowserRouter } from "react-router-dom";
+import StoreContextProvider from "./StoreContext";
 
-const store = createStore(reducers, compose(applyMiddleware(thunk)));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  
+  <BrowserRouter>
+  <StoreContextProvider>
+    <App />
+  </StoreContextProvider>
+</BrowserRouter>
 
-ReactDOM.render(
-  <Provider store={store}>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
-  </Provider>,
-  document.getElementById('root')
->>>>>>> b73f876a94c93ca105c15dc455f6b8aa4918cdba
 );
